43864dca4d82377d562cd1e7f99c9a5bd3856a3b
Angel Gerardo Jimenez Reyes <angel.g.j.reyes@outlook.com>
2021-08-03 21:08:38 -0500

    NewLaboralTask Formulario iniciado

---
+++ VisiflexAOSTUX/Controllers/LaboralTaskController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VisiflexAOSTUX.Controllers
+{
+    public class LaboralTaskController : Controller
+    {
+        // GET: LaboralTask
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewLaboralTask()
+        {
+            return View();
+        }
+    }
+}
