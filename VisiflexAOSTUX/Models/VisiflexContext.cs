c1f10e4afda69b1674841cc1d6a6f8c1e374cba2
Angel Gerardo Jimenez Reyes <angel.g.j.reyes@outlook.com>
2021-08-16 00:58:35 -0500

    Migracion EF, Clase LaboralTask, Clase Visiflexcontext

---
+++ VisiflexAOSTUX/Models/VisiflexContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace VisiflexAOSTUX.Models
+{
+    public class VisiflexContext : DbContext
+    {
+#if DEBUG
+        public VisiflexContext() : base("DefaultDebug") { }
+#else
+        public VisiflexContext() : base("Default") { }
+#endif
+        static VisiflexContext()
+        {
+            DbConfiguration.SetConfiguration(new MySql.Data.Entity.MySqlEFConfiguration());
+        }
+
+        public DbSet<LaboralTask> LaboralTasks { get; set; }
+    }
+}
