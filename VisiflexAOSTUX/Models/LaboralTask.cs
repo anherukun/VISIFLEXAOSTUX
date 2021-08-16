84b1f476ee1f15afd3474abb4966729803e27d70
Angel Gerardo Jimenez Reyes <angel.g.j.reyes@outlook.com>
2021-08-15 22:59:21 -0500

    EF6 MySQl

---
+++ VisiflexAOSTUX/Models/LaboralTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VisiflexAOSTUX.Models
+{
+    public class LaboralTask
+    {
+        public string IDLaboralTask { get; set; }
+        public string DocumentID { get; set; }
+        public DateTime DocumentDate { get; set; }
+        // [ForeingKey("IDDocumentFile")]public DocumentFile DocumentFile { get; set; }
+        // [ForeingKey("IDAttentionArea")]public AttentionArea AttentionArea { get; set; }
+        // [ForeingKey("IDRequesterArea")]public RequesterArea RequesterArea { get; set; }
+        // [ForeingKey("IDWorkplace")]public Workplace Workplace { get; set; }
+        public string IDDocumentFile { get; set; }
+        public string IDAttentionArea { get; set; }
+        public string IDRequesterArea { get; set; }
+        public string IDWorkplace { get; set; }
+        public string Subjet { get; set; }
+        public string LongDescription { get; set; }
+        public string AttentionInstructions { get; set; }
+        public string Observations { get; set; }
+
+    }
+}
---
+++ VisiflexAOSTUX/Models/VisiflexContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using MySql.Data.EntityFramework;
+using MySql.Data.MySqlClient;
+
+namespace VisiflexAOSTUX.Models
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))] public class VisiflexContext : DbContext
+    {
+#if DEBUG
+        public VisiflexContext() : base("DefaultConnectionMySQL") { }
+#else
+        public VisiflexContext() : base("DefaultConnectionMySQL-PROD") { }
+#endif
+        static VisiflexContext()
+        {
+            DbConfiguration.SetConfiguration(new MySql.Data.EntityFramework.MySqlEFConfiguration());
+        }
+
+        public DbSet<LaboralTask> LaboralTasks { get; set; }
+    }
+}
