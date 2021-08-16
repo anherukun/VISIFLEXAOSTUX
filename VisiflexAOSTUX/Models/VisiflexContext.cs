b735bfe572caaf0122443444f42131b1c796b375
Angel Gerardo Jimenez Reyes <angel.g.j.reyes@outlook.com>
2021-08-15 23:45:36 -0500

    dataentity error

--- VisiflexAOSTUX/Models/VisiflexContext.cs
+++ VisiflexAOSTUX/Models/VisiflexContext.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
-using MySql.Data.EntityFramework;
-using MySql.Data.MySqlClient;
 
 namespace VisiflexAOSTUX.Models
 {
-    [DbConfigurationType(typeof(MySqlEFConfiguration))] public class VisiflexContext : DbContext
+    public class VisiflexContext : DbContext
     {
 #if DEBUG
         public VisiflexContext() : base("DefaultConnectionMySQL") { }
-#else
+#else                         
         public VisiflexContext() : base("DefaultConnectionMySQL-PROD") { }
 #endif
         static VisiflexContext()
         {
-            DbConfiguration.SetConfiguration(new MySql.Data.EntityFramework.MySqlEFConfiguration());
+            DbConfiguration.SetConfiguration(new MySql.Data.Entity.MySqlEFConfiguration());
         }
 
         public DbSet<LaboralTask> LaboralTasks { get; set; }
