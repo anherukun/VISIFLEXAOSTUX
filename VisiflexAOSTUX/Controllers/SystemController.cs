5f1492a30842979a8906966982d37e4929e7b4cc
Angel Gerardo Jimenez Reyes <angel.g.j.reyes@outlook.com>
2021-08-16 21:33:38 -0500

    Formulario y pagina de configuraciones

---
+++ VisiflexAOSTUX/Controllers/SystemController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VisiflexAOSTUX.Controllers
+{
+    public class SystemController : Controller
+    {
+        // GET: System
+        public ActionResult Index()
+        {
+            return Redirect(Url.Action("Settings", "System"));
+        }
+
+        public ActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
