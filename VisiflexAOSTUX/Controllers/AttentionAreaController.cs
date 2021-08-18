834b1f03cc429d7b6573ca5e3141321492730a3d
Angel Gerardo Jimenez Reyes <angel.g.j.reyes@outlook.com>
2021-08-17 21:45:28 -0500

    Clase repositorio Attentionarea, Controller de Attentionarea, Modificaciones en pagina de configuraciones

---
+++ VisiflexAOSTUX/Controllers/AttentionAreaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VisiflexAOSTUX.Application;
+using VisiflexAOSTUX.Models;
+using VisiflexAOSTUX.Services;
+
+namespace VisiflexAOSTUX.Controllers
+{
+    public class AttentionAreaController : Controller
+    {
+        // GET: AttentionArea
+        public ActionResult Index()
+        {
+            return Redirect(Url.Action("", ""));
+        }
+
+        [HttpPost] public ActionResult AddAttentionArea(AttentionArea attentionArea, string controller, string action)
+        {
+            AttentionArea a = new AttentionArea()
+            {
+                AttentionAreaID = ApplicationManager.GenerateGUID,
+                AreaCode = attentionArea.AreaCode,
+                Name = attentionArea.Name
+            };
+
+            RepositoryAttentionArea.Add(a);
+
+            return Redirect(Url.Action(action, controller));
+        }
+    }
+}
---
+++ VisiflexAOSTUX/Services/RepositoryAttentionArea.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using VisiflexAOSTUX.Models;
+
+namespace VisiflexAOSTUX.Services
+{
+    public class RepositoryAttentionArea
+    {
+        public static int Add(AttentionArea a)
+        {
+            using (var db = new VisiflexContext())
+            {
+                db.AttentionAreas.AddOrUpdate(a);
+                return db.SaveChanges();
+            }
+        }
+
+        public static List<AttentionArea> Get()
+        {
+            using (var db = new VisiflexContext())
+            {
+                return db.AttentionAreas.OrderBy(x => x.AreaCode).ToList();
+            }
+        }
+        public static AttentionArea Get(string id)
+        {
+            using (var db = new VisiflexContext())
+            {
+                return db.AttentionAreas.Where(x => x.AttentionAreaID == id).FirstOrDefault();
+            } 
+        }
+
+        public static void Delete(string id)
+        {
+            using (var db = new VisiflexContext())
+            {
+                db.Entry(Get(id)).State = System.Data.Entity.EntityState.Deleted;
+                db.SaveChanges();
+            }
+        }
+
+        public static bool Exist(AttentionArea a)
+        {
+            using (var db = new VisiflexContext())
+            {
+                return db.AttentionAreas.Any(x => x.AreaCode == a.AreaCode );
+            }
+        }
+    }
+}
