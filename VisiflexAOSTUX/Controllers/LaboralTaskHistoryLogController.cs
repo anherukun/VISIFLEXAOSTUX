ab82615fb46bbb4d3fc9fb53fc6d5cc2d67c8b8f
Angel Gerardo Jimenez Reyes <angel.g.j.reyes@outlook.com>
2021-08-30 14:56:24 -0500

    + Vista history Log

---
+++ VisiflexAOSTUX/Controllers/LaboralTaskHistoryLogController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VisiflexAOSTUX.Models;
+using VisiflexAOSTUX.Services;
+
+namespace VisiflexAOSTUX.Controllers
+{
+    public class LaboralTaskHistoryLogController : Controller
+    {
+        // GET: LaboralTaskHistoryLog
+        public ActionResult Index()
+        {
+            return Redirect(Url.Action("", ""));
+        }
+
+        public ActionResult HistoryLog(string laboralTaskID, ResponseMessage response)
+        {
+            ViewData["laboralTask"] = RepositoryLaboralTask.Get(laboralTaskID);
+            ViewData["laboralTaskHystoryLog"] = RepositoryLaboralTaskHistoryLog.Get(laboralTaskID);
+            ViewData["Response"] = response;
+
+            return View();
+        }
+    }
+}
